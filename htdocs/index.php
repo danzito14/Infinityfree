331d997f9cc9394db3adda328c37540f3dca9216
Uriel Alonso <urielalonsocaro@outlook.com>
2025-02-11 20:56:41 -0700

    Se subio todo

---
+++ htdocs/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+    <title>Contenedor de tareas</title> <!-- Titulo de la ventana-->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <link rel="stylesheet" href="/sitepro/css/cont_d_tareas.css"> <!-- Origen de estilos-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+</head>
+
+<body>
+
+    <header>
+        <h1>Mi contenendor de tareas</h1>
+    </header>
+
+    <main>
+        <div class="contenedor_de_tareas">
+            <div>
+                <h2>Tarea 1</h2>
+                <p>Se creará
+                    una página web que contendrá enlaces a las tareas elaboradas para su revisión
+                    (solo páginas web), de tal forma que todas las tareas se encuentren en una sola
+                    página.</p>
+                <p><b>Link: </b><a href="#">Enlace a la tarea 1</a></p>
+            </div>
+
+            <div>
+                <h2>Tarea 1</h2>
+                <p>Se creará
+                    una página web que contendrá enlaces a las tareas elaboradas para su revisión
+                    (solo páginas web), de tal forma que todas las tareas se encuentren en una sola
+                    página.</p>
+                <p><b>Link: </b><a href="#">Enlace a la tarea 1</a></p>
+            </div>
+        </div>
+        <div class="Perfil">
+            <h2> Mis Datos</h2>
+            <p> <strong>Nombre: </strong> Caro Diaz Uriel Alonso</p>
+            <p><b>Grupo:</b> 301</p>
+            <img id="foto_de_perfil" src="/sitepro/gallery/Danzito.jpg" alt="img">
+        </div>
+    </main>
+
+    <footer>
+        <h3>Elaborado por: Caro Diaz Uriel Alonso</h3>
+    </footer>
+</body>
+
+</html>
